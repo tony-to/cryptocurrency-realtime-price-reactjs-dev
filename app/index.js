ddb4d9bd393eb81cd657f126f40e2886509f9329
Tony To <chungtcc@gmail.com>
2018-02-07 20:27:38 +0800

    Remove all hardcoded logic and data in react layer

--- app/index.js
+++ app/index.js
@@ -3,38 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-
-// const API_URL = "https://api.cryptonator.com/api/ticker/";
-const API_URL = "http://localhost:3000/pairs/";
+const API_URL = "http://localhost:3000";
 
 const UPDATE_INTERVAL = 10000;
 
-const pairs = [
-  {"name":"Bitcoin","id":"btc-usd"},
-  {"name":"Ether","id":"eth-usd"},
-  {"name":"Litecoin","id":"ltc-usd"},
-  {"name":"Monero","id":"xmr-usd"},
-  {"name":"Ripple","id":"xrp-usd"},
-  {"name":"Dogecoin","id":"doge-usd"},
-  {"name":"Dash","id":"dash-usd"},
-  {"name":"MaidSafeeCoin","id":"maid-usd"},
-  {"name":"Lisk","id":"lsk-usd"},
-  {"name":"Storjcoin X","id":"sjcx-usd"}
-]
-
-//For Testing
-const test_data = [
-    {"name":"Bitcoin","base":"BTC","target":"USD","price":"8477.42028862","volume":"92916.88297066","change":"44.09777279"},
-    {"name":"Ether","base":"ETH","target":"USD","price":"876.51060450","volume":"310480.13154134","change":"14.15302330"},
-    {"name":"Litecoin","base":"LTC","target":"USD","price":"152.39716697","volume":"1031874.98751268","change":"1.12432456"},
-    {"name":"Monero","base":"XMR","target":"USD","price":"221.12713565","volume":"52618.16494258","change":"2.10359105"},
-    {"name":"Ripple","base":"XRP","target":"USD","price":"0.83239443","volume":"190525567.50193000","change":"0.01127935"},
-    {"name":"Dogecoin","base":"DOGE","target":"USD","price":"0.00446731","volume":"169185147.28885001","change":"0.00003487"},
-    {"name":"Dash","base":"DASH","target":"USD","price":"555.20835131","volume":"21771.57603766","change":"4.67114421"},
-    {"name":"MaidSafeeCoin","base":"MAID","target":"USD","price":"0.38887560","volume":"","change":"0.00273993"},
-    {"name":"Lisk","base":"LSK","target":"USD","price":"20.17610000","volume":"1511.00000000","change":"0.00000000"},
-    {"name":"Storjcoin X","base":"","target":"","price":"","volume":"","change":""}
-];
 
 class Header extends React.Component {
   constructor(props) {
@@ -66,39 +38,15 @@ class App extends React.Component {
     };
   }
 
-
   componentDidMount () {
-    var results = [];
     var _this = this;
-    pairs.map(function(pair, i){
-      axios.get(API_URL+pair.id, {
-          responseType: 'json'
-      }).then(response => {
-        //Used for directly call api in react UI
-        // if(response.data.success){
-        //   var ticker = response.data.ticker;
-        //   ticker.name = pair.name;
-        //   results.push(ticker);
-        // }else{
-        //   var ticker = {"base":"-","target":"-","price":"-","volume":"-","change":"-"};
-        //   ticker.name = pair.name;
-        //   results.push(ticker);
-        // }
-        var result = response.data;
-        result.name = pair.name;
-        results.push(response.data);
-        if(i == (pairs.length - 1)){
-          _this.setState({boxes: results});
-          ReactDOM.render(<Timer />, document.getElementById('update-time'));
-        }
-      });
-
-      //For fake testing
-      // results.push(test_data[i]);
-      // if(i == (pairs.length - 1)){
-      //   _this.setState({boxes: results});
-      // }
-    })
+    axios.get(API_URL+"/pairs", {
+        responseType: 'json'
+    }).then(response => {
+      var results = response.data;
+      _this.setState({boxes: results});
+      ReactDOM.render(<Timer />, document.getElementById('update-time'));
+    });
   }
 
 
